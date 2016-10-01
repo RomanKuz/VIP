809c2b848a17e0027c85a0a19d8c843bbe5d5abf
Bohdan Zhmud <bzhmud@softserveinc.com>
2016-10-01 14:47:03 +0300

    fixed references

--- src/WordsComp/wwwroot/tscripts/interfaces.ts
+++ src/WordsComp/wwwroot/tscripts/interfaces.ts
@@ -1,1 +1,3 @@
 /// <reference path="./interfaces/iConnectToGame.ts" />
+/// <reference path="./interfaces/iConnectToGameScope.ts" />
+/// <reference path="./interfaces/iHubConnection.ts" />
--- src/WordsComp/wwwroot/tscripts/models/group.ts
+++ src/WordsComp/wwwroot/tscripts/models/group.ts
@@ -1,7 +1,8 @@
+/// <reference path="./User.ts" />
 module Models {
     export class Group {
         public groupId: string;
 
-        public usersList: Array<User>
+        public usersList: Array<Models.User>
     }
 }
